05fe29f73058832c424b28891b7d4fc990888f9b
ioann <ivan.karmalyk2017@gmail.com>
2022-12-29 12:21:28 +0200

    optimize parse circle

--- vendor/Services/Service.php
+++ vendor/Services/Service.php
@@ -7,25 +7,37 @@ class Service
     static public function arrayCovert(array $array): array
     {
         $resArr = [];
-
+        $resArrAnswer = [];
         foreach ($array as $value){
             $resArr[$value->quest_id] = [
                 'text'=>$value->qustions,
                 'published'=>$value->published,
                 'date'=>$value->date,
             ];
-        }
-
-        foreach ($array as $value){
             if (!empty($value->answer_id)){
-                $resArr[$value->quest_id]['answers'][$value->answer_id] = [
+                $resArrAnswer[$value->quest_id]['answers'][$value->answer_id] = [
                     'answer'=>$value->answer,
                     'voices'=>$value->voices,
                     'update'=>1,
                 ];
             }
+
+            $resArr[$value->quest_id]['answers'] = $resArrAnswer[$value->quest_id]['answers'];
+
         }
 
+//        foreach ($array as $value){
+//            if (!empty($value->answer_id)){
+//                $resArr[$value->quest_id]['answers'][$value->answer_id] = [
+//                    'answer'=>$value->answer,
+//                    'voices'=>$value->voices,
+//                    'update'=>1,
+//                ];
+//            }
+//        }
+//        echo "<pre>".print_r($resArr,1)."</pre>";
+//        echo "<pre>".print_r($resArrAnswer,1)."</pre>";
+//        exit();
         return $resArr;
     }
 }
